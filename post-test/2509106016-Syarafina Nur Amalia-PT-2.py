f14298bd2b1fda67b208e01c1aab9966e80768d6
senku_maniac <devanoargya6@gmail.com>
2025-09-23 17:39:50 +0800

    PT2

---
+++ post-test/2509106016-Syarafina Nur Amalia-PT-2.py
@@ -0,0 +1,24 @@
+'''Hasil Cek Berat Badan'''
+
+from prettytable import PrettyTable
+from colorama import Fore, Style
+
+t = PrettyTable()
+t.field_names = ['keterangan', 'Hasil']
+
+nama = input('Masukkan nama Anda = ')
+tinggiBadan = float(input('Masukkan tinggi badan Anda = '))
+beratBadan = float(input('Masukkan berat badan Anda = '))
+
+beratIdeal = (tinggiBadan - 100)
+isKelebihan = beratBadan > beratIdeal
+
+statusTubuh = ['Berat badan ideal', 'Kelebihan berat badan, perbanyak olahraga']
+status = statusTubuh[int(isKelebihan)]
+
+print(Fore.CYAN + '\n\n','='*15,'Status Kesehatan Anda','='*15)
+t.add_row(['Nama', Fore.BLUE + nama])
+t.add_row(['Tinggi Badan (cm)', tinggiBadan])
+t.add_row(['Berat Badan (kg)', beratBadan])
+t.add_row(['Status', status])
+print(t)
