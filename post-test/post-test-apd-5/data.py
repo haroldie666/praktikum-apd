6bd9dbc90909db8406fc2da4dfc00ca52efa1986
senku_maniac <devanoargya6@gmail.com>
2025-10-15 07:30:57 +0800

    PT5

---
+++ post-test/post-test-apd-5/data.py
@@ -0,0 +1,14 @@
+users = [
+    ['admin', 'admin123', 'admin'],
+    ['fina', '016', 'admin'],
+    ['senku', '100billion', 'admin'],
+    ['kohaku', 'power', 'user'],
+    ['denji', 'chainsaw', 'user'],
+]
+
+buku_sementara = [
+    [1, 'Laut Bercerita', 'Leila S.', 128000, 18, 'Novel'],
+    [2, 'Namaku Alam', 'Leila S.',157000, 10, 'Novel'],
+    [3, 'Bumi Manusia', 'Pramoedya Ananta Toer', 231000, 12, 'Novel'],
+    [4, 'Dunia Sophie', 'Jostein Gaarder', 267000, 16, 'Novel'],
+]
